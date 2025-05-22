f872aec0225e01f7cb441ea4aa268b580a9b2137
Varun Kandhari <75046190+Varun612@users.noreply.github.com>
2025-05-22 17:23:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // Your code here.
+const items=document.querySelectorAll('.items');
+let isDragging = false;
+let currentItem;
+items.forEach((item,index) => {
+	// const item = document.querySelector('.item');
+	item.addEventListener('mousedown', (e) => {
+		isDragging=true;
+		currentItem=item;
+	});
+		
+});
+document.addEventListener('mousemove', (e) => {
+	if(isDragging && currentItem){
+		currentItem.style.left = e.pageX + 'px';
+		currentItem.style.top = e.pageY + 'px';
+	}
+});
+document.addEventListener('mouseup', () => {
+	isDragging=false;
+	currentItem=null;
+});
