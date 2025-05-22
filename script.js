84d4065689aafffdad913a05105685ae39a83336
Varun Kandhari <75046190+Varun612@users.noreply.github.com>
2025-05-22 17:46:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 // Your code here.
-const items=document.querySelectorAll('.items');
+const items=document.querySelectorAll('.item');
 let isDragging = false;
-let currentItem;
-items.forEach((item,index) => {
+let currentItem, offsetX, offsetY;
+items.forEach((item) => {
 	// const item = document.querySelector('.item');
 	item.addEventListener('mousedown', (e) => {
 		isDragging=true;
 		currentItem=item;
+		offsetX = e.clientX-currentItem.getBoundingClientRect().left;
+		offsetY = e.clientY-currentItem.getBoundingClientRect().top;
+
+		currentItem.classList.add("active");
 	});
 		
 });
 document.addEventListener('mousemove', (e) => {
 	if(isDragging && currentItem){
-		currentItem.style.left = e.pageX + 'px';
-		currentItem.style.top = e.pageY + 'px';
+		currentItem.style.left = (e.clientX-offsetX) + 'px';
+		currentItem.style.top = (e.clientY-offsetY) + 'px';
 	}
 });
 document.addEventListener('mouseup', () => {
+	if(currentItem) {
+		currentItem.classList.remove('active');
+	}
 	isDragging=false;
 	currentItem=null;
 });
